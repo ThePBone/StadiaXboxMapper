e44f634c5a5fa20d614fc5e2398f8a1c2180b236
ThePBone <thebone.main@gmail.com>
2020-12-29 20:56:50 +0100

    Add more actions

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ Launch it:
 ```
 python3 xbox_map_service.py
 ```
+
+## Behaviour
+
+| Keycombo                                 	| Action                             	|
+|------------------------------------------	|------------------------------------	|
+| Hold L and R stick down for 0.25 seconds 	| Take screenshot                    	|
+| Hold L and R stick down for 1 second     	| Record last 30 seconds of gameplay 	|
+| Hold L and R stick down for 2 seconds    	| Exit game                          	|
+| Press Xbox button                        	| Open Stadia's in-game menu bar     	|
--- xbox_map_service.py
+++ xbox_map_service.py
@@ -1,5 +1,36 @@
 import evdev
 import pyautogui
+import time
+
+leftStickDown = False
+rightStickDown = False
+
+bothDownStartTime = -1
+
+
+def processStickDownTime(seconds):
+    if seconds > 2:
+        pyautogui.hotkey("esc")
+    elif seconds > 1:
+        pyautogui.keyDown('f12')
+        time.sleep(1)
+        pyautogui.keyUp('f12')
+    elif seconds > 0.25:
+        pyautogui.hotkey("f12")
+
+
+def checkState():
+    global bothDownStartTime
+    if leftStickDown and rightStickDown:
+        if bothDownStartTime < 0:
+            print("Both sticks held down")
+            bothDownStartTime = time.time()
+    elif bothDownStartTime > 0:
+        diff = time.time() - bothDownStartTime
+        bothDownStartTime = -1
+        processStickDownTime(diff)
+        print("Held down for", round(diff, 2), "seconds")
+
 
 dev = None
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
@@ -14,6 +45,12 @@ if dev is None:
     exit(1)
 
 for event in dev.read_loop():
-    if event.code == evdev.ecodes.KEY_HOMEPAGE and event.value == 1:
+    if event.code == 317:
+        leftStickDown = event.value
+    elif event.code == 318:
+        rightStickDown = event.value
+    elif event.code == evdev.ecodes.KEY_HOMEPAGE and event.value == 1:
         pyautogui.hotkey('shift', 'tab')
-        print("Event fired")
+        print("Xbox button pressed")
+
+    checkState()
